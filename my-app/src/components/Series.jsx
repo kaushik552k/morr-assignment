9e13edbb4d53d9cbc908efd47de5347ef3b45a04
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-06-07 10:51:53 +0530

    created series component

---
+++ my-app/src/components/Series.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Card from './cards/Card'
+import Data from './cards/Data'
+import './movies.css'
+import Footer from './Footer';
+import Header from './Header';
+
+
+
+export default function Series() {
+    return (
+        <>
+        <Header />
+            <h6 className="heading_style">Popular Series </h6>
+
+            {Data.map((val) => {
+                return (
+                    <Card
+                        imgsrc={val.images}
+                        title={val.title}
+                        sname={val.description}
+                        releaseYear={val.releaseYear}
+                    />
+                )
+            })}
+
+            <Footer/>
+        </>
+
+    )
+}
+
+
