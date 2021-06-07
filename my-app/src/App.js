eb0ee05cad777a63fe12c82e8ca591fe6d56d7bd
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-06-07 11:41:55 +0530

    final commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Movies from './components/Movies'
+import Home from './components/Home'
+import Series from './components/Series';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/movies" component={Movies}></Route>
+        <Route exact path="/series" component={Series}></Route>
+        <Home/>
+
+      </Switch>
     </div>
+  </Router>
+
   );
 }
 
